abb0c01b75ec8fd287fd4e0d9f75748430626325
Cooleshov <cooleshov@internet.ru>
2023-07-26 13:04:06 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./scss/style.scss"
+import "./index.html"
+
+for (let index = 0; index < 10; index++) {
+    console.log(index)
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // fallback to style-loader in development
+                    process.env.NODE_ENV !== "production"
+                        ? "style-loader"
+                        : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: false,
+        liveReload: true,
+    },
+    cache: true,
+    mode: "development",
+}
