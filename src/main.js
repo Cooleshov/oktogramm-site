708ba28ca075bf7ef5334e604993bd792f7a9542
Cooleshov <cooleshov@internet.ru>
2023-08-02 15:17:22 +0300

    add networking and mentor sections

--- src/main.js
+++ src/main.js
@@ -5,87 +5,39 @@ import { TextPlugin } from "gsap/TextPlugin"
 
 gsap.registerPlugin(TextPlugin)
 
-let states = {
-	scopes: {
-		block1: {
-			class: ".scope__image--1",
-			button: ".scope__button-1",
-			state: false,
-		},
-		block2: {
-			class: ".scope__image--2",
-			button: ".scope__button-2",
-			state: false,
-		},
-		block3: {
-			class: ".scope__image--3",
-			button: ".scope__button-3",
-			state: false,
-		},
-		block4: {
-			class: ".scope__image--4",
-			button: ".scope__button-4",
-			state: false,
-		},
-		block5: {
-			class: ".scope__image--5",
-			button: ".scope__button-5",
-			state: false,
-		},
-	},
-}
+const scopeCards = document.querySelectorAll(".scope__description")
+let scopeStates = [false, false, false, false, false]
 
-function toggleHide(element, object) {
-	let cssClass = object[element].class
-	let state = object[element].state
+scopeCards.forEach(function (item, index) {
+	const timeline = gsap
+		.timeline({ paused: true })
+		.to(item.querySelector(".scope__image"), { duration: 0.25, autoAlpha: 0 })
 
-	if (state) {
-		gsap.to(cssClass, {
-			duration: 0.25,
-			autoAlpha: 1,
+	const button = gsap
+		.timeline({ paused: true })
+		.set(item.querySelector(".scope__button p"), {
+			duration: 0,
+			text: "Понятно",
 		})
-		object[element].state = false
-	} else {
-		gsap.to(cssClass, {
-			duration: 0.25,
-			autoAlpha: 0,
-		})
-		object[element].state = true
-	}
-}
 
-function changeButtonText(element, object) {
-	let cssClass = object[element].button + " p"
-	let state = object[element].state
+	item.querySelector(".scope__button").addEventListener("click", function () {
+		if (scopeStates[index] === false) {
+			timeline.play()
+			button.play()
 
-	if (state) {
-		gsap.set(cssClass, { text: "Понятно" })
-	} else {
-		gsap.set(cssClass, { text: "Подробнее" })
-	}
-}
+			scopeStates[index] = true
+		} else {
+			timeline.reverse()
+			button.reverse()
 
-function scopeToggle(element, object) {
-	toggleHide(element, object)
-	changeButtonText(element, object)
+			scopeStates[index] = false
+		}
 
-	let plus = document.querySelector(
-		object[element].button + " .scope__button-image:nth-child(1)"
-	)
-	let chechmark = document.querySelector(
-		object[element].button + " .scope__button-image:nth-child(2)"
-	)
-	plus.classList.toggle("scope__button--disabled")
-	chechmark.classList.toggle("scope__button--disabled")
-}
-
-document.querySelector(".scope__button-1").onclick = () =>
-	scopeToggle("block1", states.scopes)
-document.querySelector(".scope__button-2").onclick = () =>
-	scopeToggle("block2", states.scopes)
-document.querySelector(".scope__button-3").onclick = () =>
-	scopeToggle("block3", states.scopes)
-document.querySelector(".scope__button-4").onclick = () =>
-	scopeToggle("block4", states.scopes)
-document.querySelector(".scope__button-5").onclick = () =>
-	scopeToggle("block5", states.scopes)
+		item
+			.querySelector(".scope__button-image:nth-child(1)")
+			.classList.toggle("scope__button--disabled")
+		item
+			.querySelector(".scope__button-image:nth-child(2)")
+			.classList.toggle("scope__button--disabled")
+	})
+})
